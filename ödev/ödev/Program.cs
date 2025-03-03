96b5e743e6aa05e4391dcb60a555e0e70967ab55
shintaiyou <kaancnbz004@gmail.com>
2025-03-03 14:15:22 +0300

    ödev1

---
+++ ödev/ödev/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using ödev.classes;
+
+Audi a = new Audi();
+a.marka = "Audi";
+a.vitesTuru = "otomatik vitesli";
+a.yazdir(a.marka, a.vitesTuru);
+Bmw b = new Bmw();
+b.marka = "bmw";
+b.vitesTuru = "manuel vitesli";
+b.yazdir(b.marka, b.vitesTuru);
+Mercedes m = new Mercedes();
+m.marka = "mercedes";
+m.vitesTuru = "manuel vitesli";
+m.yazdir(m.marka, m.vitesTuru);
+Nissan n = new Nissan();
+n.marka = "nissan";
+n.vitesTuru = "manuel vitesli";
+n.yazdir(n.marka, n.vitesTuru);
+Porsche p = new Porsche();
+p.marka = "porsche";
+p.vitesTuru = "manuel vitesli";
+p.yazdir(p.marka, p.vitesTuru);
+Togg t = new Togg();
+t.marka = "togg";
+t.vitesTuru = "otomatik vitesli";
+t.yazdir(t.marka, t.vitesTuru);
+
+
---
+++ ödev/ödev/classes/araba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev.classes
+{
+    public class Araba
+    { 
+        public string marka;
+        public string vitesTuru;
+        public void yazdir (string marka, string vitesTuru)
+        {
+            Console.WriteLine(marka + " -> " + vitesTuru);
+
+        }
+    }
+}
