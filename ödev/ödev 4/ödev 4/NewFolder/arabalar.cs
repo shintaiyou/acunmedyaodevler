39db2bdc5394ce935c42789e171725ff04d7b760
shintaiyou <kaancnbz004@gmail.com>
2025-03-07 17:54:37 +0300

    ödev 3 yapıldı

---
+++ ödev/ödev 4/ödev 4/NewFolder/arabalar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ödev_4.NewFolder
+{
+    public class arabalar
+    {
+        public string araç;
+
+        public double litre;
+        public string li;
+        public void yazdır(string araç,double litre,string li)
+        {
+            Console.WriteLine( araç + " "+ "->"+ " " +  litre + li);
+        }
+    }
+}
---
+++ ödev/ödev 4/ödev 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using ödev_4.NewFolder;
+
+bmw b = new bmw();
+b.araç = "bmw";
+b.litre = 80.0;
+b.li = "L";
+b.yazdır(b.araç, b.litre,b.li);
+mercedes m  = new mercedes();
+m.araç = "mercedes";
+m.litre = 70.0;
+m.li = "L";
+m.yazdır(m.araç, m.litre,m.li);
+porsche p = new porsche();
+p.araç = "porsche";
+p.litre = 60.0;
+p.li = "L";
+p.yazdır(p.araç, p.litre,p.li);
